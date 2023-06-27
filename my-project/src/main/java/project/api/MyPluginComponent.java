07b6db23d312a170106dd62d5108f6314be504b1
manideep53 <manideepsaiyadav@gmail.com>
2023-06-27 19:10:40 +0530

    Jira project

---
+++ my-project/src/main/java/project/api/MyPluginComponent.java
@@ -0,0 +1,6 @@
+package project.api;
+
+public interface MyPluginComponent
+{
+    String getName();
+}
---
+++ my-project/src/test/java/ut/project/MyComponentUnitTest.java
@@ -0,0 +1,17 @@
+package ut.project;
+
+import org.junit.Test;
+import project.api.MyPluginComponent;
+import project.impl.MyPluginComponentImpl;
+
+import static org.junit.Assert.assertEquals;
+
+public class MyComponentUnitTest
+{
+    @Test
+    public void testMyName()
+    {
+        MyPluginComponent component = new MyPluginComponentImpl(null);
+        assertEquals("names do not match!", "myComponent",component.getName());
+    }
+}
